69b80b44445697a7a232b98a8ee8778e4c55d045
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-25 01:38:53 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,14 +7,14 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories, :reject_if => proc { |attrs| attrs[:name].blank? }
 
 
-def categories_attributes=(attributes)
-  attributes.values.each do |attribute|
-    if attribute[:name].present?
-      category = Category.find_or_create_by(attribute)
-      self.categories << category
+  def categories_attributes=(attributes)
+    attributes.values.each do |attribute|
+      if attribute[:name].present?
+        category = Category.find_or_create_by(attribute)
+        self.categories << category
+      end
     end
   end
-end
 
 
 end
