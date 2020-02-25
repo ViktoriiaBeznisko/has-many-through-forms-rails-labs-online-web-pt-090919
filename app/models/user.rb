41eb77c9054c964395f64d8f8f532220ca5b6cbf
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-25 01:39:30 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
   
-    def post_comments(post)
+  def post_comments(post)
     post = Post.find(post.id)
     post.comments.select { |comment| comment.user_id == self.id }
   end
