8dd7d10d41a2fba67aa1b84390b33b7036f014da
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-25 01:37:02 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,6 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
     
-    compar = params.require(:comment)
-    if compar[:user_attributes][:username].length > 0
-      compar.permit(:content, :post_id, user_attributes:[:username])
-    else
-      compar.permit(:content, :post_id, :user_id)
-    end
+    
   end
 end
